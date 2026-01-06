a2c38de1d3b2d616757fbb006fca415cf03df301
fpbismyname <fajarpebefajar12@gmail.com>
2026-01-06 16:53:08 +0700

    Hanifa update v16

--- config/site.php
+++ config/site.php
@@ -41,26 +41,4 @@ return [
         'address' => 'Jl. Raya Cianjur No. 123, Cianjur, Jawa Barat',
         'whatsapp' => 'https://wa.me/+6283181686431'
     ],
-    'prosedur_pengajuan_surat_kontrak' => <<<TEXT
-    Prosedur pengajuan kontrak kerja
-    - Download surat kontrak kerja.
-    - Pastikan kualifikasi pelatihan anda sesuai dengan kategori kontrak kerja.
-    - Edit surat pengajuan kontrak kerja, Isi nama dan tanda tangan anda.
-    - Kirim surat pengajuan kontrak kerja pada form dibawah.
-    TEXT,
-    'ujian_bahasa' => [
-        'N4' => ['Kosakata', 'Tata bahasa', 'Pengetahuan bahasa', 'Membaca', 'Mendengar'],
-        'N3' => ['Kosakata', 'Tata bahasa', 'Pengetahuan bahasa', 'Membaca', 'Mendengar'],
-        'N2' => ['Kosakata', 'Tata bahasa', 'Pengetahuan bahasa', 'Membaca', 'Mendengar'],
-        'N1' => ['Kosakata', 'Tata bahasa', 'Pengetahuan bahasa', 'Membaca', 'Mendengar'],
-    ],
-    'ujian_softskill' => [
-        'Disiplin & ketepatan waktu',
-        'Sikap kerja',
-        'Ketaatan SOP & Keselamatan',
-        'Komunikasi dasar tempat kerja',
-        'Kerja tim',
-        'Etika & Budaya kerja',
-        'Manajemen Emosi & Stress'
-    ]
 ];
